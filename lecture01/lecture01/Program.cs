0506f1e587195e47ede378d304af428834719a2a
mykhaylo zhovkevych <148889468+mykhaylo-zhovkevych@users.noreply.github.com>
2025-03-19 15:16:09 +0100

    test commit

---
+++ lecture01/lecture01/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lecture01
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("?=23_§);");
+        }
+    }
+}

637ea731969f21e6ed621e5ccd1bb743d0aa90a6
Mykhaylo Zhovkevych <mykhaylo.zhovkevych@variosystems.com>
2025-09-10 17:04:54 +0200

    qc #5

---
+++ work/Training/EventDemo/Program.cs
@@ -0,0 +1,93 @@
+﻿namespace EventDemo
+{
+    public class PetEventArgs : EventArgs
+    {
+        public string Message { get; }
+        public DateTime Time { get; }
+
+        public PetEventArgs(string message)
+        {
+            Message = message;
+            Time = DateTime.Now;
+        }
+    }
+
+    // Publishers (Sensorens)
+    public class FoodSensor
+    {
+        public event EventHandler<PetEventArgs> FoodLow;
+
+        public void CheckFoodLevel(int foodAmount)
+        {
+            if (foodAmount < 20)
+            {
+                OnFoodLow(new PetEventArgs("Food is running low"));
+            }
+        }
+        protected virtual void OnFoodLow(PetEventArgs e)
+        {
+            FoodLow?.Invoke(this, e);
+        }
+
+    }
+
+    public class WaterSensor
+    {
+        public event EventHandler<PetEventArgs> WaterLow;
+
+        public void CheckWaterLevel(int waterAmount)
+        {
+            if (waterAmount < 10)
+            {
+                OnWaterLow(new PetEventArgs("Water is almost empty"));
+            }
+        }
+
+        protected virtual void OnWaterLow(PetEventArgs e)
+        {
+            WaterLow?.Invoke(this, e);
+        }
+
+    }
+
+    // Subscribers
+    public class OwnerNotifier
+    {
+        public void OnEventReceived(object sender, PetEventArgs e)
+        {
+            Console.WriteLine($"[Notification to Owner] {e.Message} at {e.Time}");
+        }
+    }
+
+    public class Logger
+    {
+        public void OnEventreceived(object sender, PetEventArgs e)
+        {
+            Console.WriteLine($"[Log] {sender.GetType().Name}: {e.Message}");
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var foodSensor = new FoodSensor();
+            var waterSensor = new WaterSensor();
+
+            var ownerNotifier = new OwnerNotifier();
+            var logger = new Logger();
+
+
+            foodSensor.FoodLow += ownerNotifier.OnEventReceived;
+            foodSensor.FoodLow += logger.OnEventreceived;
+
+            waterSensor.WaterLow += ownerNotifier.OnEventReceived;
+            waterSensor.WaterLow += logger.OnEventreceived;
+
+            // Simulation
+            foodSensor.CheckFoodLevel(14);
+            waterSensor.CheckWaterLevel(5);
+        }
+    }
+}
---
+++ work/Training/LINQDemo/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace LINQDemo
+{
+
+
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var individuals = new List<Person>()
+            {
+                new Person { Name = "Anna", Age = 34},
+                new Person { Name = "Bernd", Age = 40 },
+                new Person { Name = "Clara", Age = 30 }
+
+            };   
+            
+            var ab30 = from i in individuals
+                       where i.Age >= 30
+                       orderby i.Age
+                       select i;
+
+            var ab30Method = individuals
+                            .Where(i => i.Age >= 30)
+                            .OrderBy(i => i.Age);
+
+            // Or ab30Method
+            foreach (var i in ab30)
+            {
+                Console.WriteLine($"{i.Name} ({i.Age})");
+            }
+
+        }
+    }
+}

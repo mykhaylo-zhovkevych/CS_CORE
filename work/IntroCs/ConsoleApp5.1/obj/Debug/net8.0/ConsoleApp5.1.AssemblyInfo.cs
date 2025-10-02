27bb0df3739f5ee315b9115cfa8c06c5aa7f9da0
mykhaylo zhovkevych <148889468+mykhaylo-zhovkevych@users.noreply.github.com>
2025-10-02 08:03:47 +0200

    temp

--- work/IntroCs/ConsoleApp5.1/obj/Debug/net8.0/ConsoleApp5.1.AssemblyInfo.cs
+++ work/IntroCs/ConsoleApp5.1/obj/Debug/net8.0/ConsoleApp5.1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp5.1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+212f1284f38c3c78da78c52a2333939a1230e562")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7dc809353a18d325843d56a8c2946398d40d7f87")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp5.1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp5.1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

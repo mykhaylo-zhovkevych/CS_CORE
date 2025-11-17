31a9ac14e4be061f2c937c79c717c775d66cdf3b
mykhaylo zhovkevych <148889468+mykhaylo-zhovkevych@users.noreply.github.com>
2025-11-17 23:15:14 +0100

    qc

--- work/Training/WebApplicationExample/Controllers/ShopController.cs
+++ work/Training/WebApplicationExample/Controllers/ShopController.cs
@@ -20,7 +20,11 @@ namespace WebApplicationExample.Controllers
                 Age = 30,
                 Email = "example@email.com",
                 PhoneNumber = "123-456-7890",
-                Address = "123 Main St, Cityville",
+                Addresses = new List<Customer.Address>
+                {
+                    new Customer.Address { Id = 1, Street = "St Main", City = "NewYork" },
+                    new Customer.Address { Id = 2, Street = "St Quie", City = "Geneve" }
+                },
                 contacts = new List<Contact>
                 {
                     new Contact
@@ -43,7 +47,11 @@ namespace WebApplicationExample.Controllers
                 Age = 20,
                 Email = "example@email.com",
                 PhoneNumber = "123-456-7890",
-                Address = "123 Main St, Cityville"
+                Addresses = new List<Customer.Address>
+                {
+                    new Customer.Address { Id = 1, Street = "St Main", City = "NewYork" },
+                    new Customer.Address { Id = 2, Street = "St Quie", City = "Geneve" }
+                },
             },
             new Customer
             {
@@ -52,7 +60,11 @@ namespace WebApplicationExample.Controllers
                 Age = 40,
                 Email = "example@email.com",
                 PhoneNumber = "123-456-7890",
-                Address = "123 Main St, Cityville"
+                Addresses = new List<Customer.Address>
+                {
+                    new Customer.Address { Id = 1, Street = "St Main", City = "NewYork" },
+                    new Customer.Address { Id = 2, Street = "St Quie", City = "Geneve" }
+                },
             },
             new Customer
             {
@@ -61,7 +73,11 @@ namespace WebApplicationExample.Controllers
                 Age = 30,
                 Email = "example@email.com",
                 PhoneNumber = "123-456-7890",
-                Address = "123 Main St, Cityville"
+                Addresses = new List<Customer.Address>
+                {
+                    new Customer.Address { Id = 1, Street = "St Main", City = "NewYork" },
+                    new Customer.Address { Id = 2, Street = "St Quie", City = "Geneve" }
+                },
             }
 
         };
@@ -101,8 +117,38 @@ namespace WebApplicationExample.Controllers
             return CreatedAtAction(nameof(GetCustomerId), new { id = customer.Id }, newContact);
         }
 
+        // Put /api/customers/{id}
+        [HttpPut("{id}")]
+        public ActionResult UpdateCustomer(int id, [FromBody] Customer updatedCustomer)
+        {
+            var customer = _customers.FirstOrDefault(c => c.Id == id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
 
+            customer.Name = updatedCustomer.Name;
+            customer.Age = updatedCustomer.Age;
+            customer.Email = updatedCustomer.Email;
+            customer.PhoneNumber = updatedCustomer.PhoneNumber;
+            customer.Addresses = updatedCustomer.Addresses;
 
+            return NoContent();
 
+        }
+
+        // DELETE /api/customers/{customerId}/addresses/{addressId}
+        [HttpDelete("{customerId}/addresses/{addressId}")]
+        public ActionResult DeleteCustomerAddress(int customerId, int addressId)
+        {
+            var customer = _customers.FirstOrDefault(c => c.Id == customerId);
+            if (customer == null) return NotFound();
+
+            var address = customer.Addresses.FirstOrDefault(a => a.Id == addressId);
+            if (address == null) return NotFound();
+
+            customer.Addresses.Remove(address);
+            return NoContent();
+        }
     }
 }
--- work/Training/WebApplicationExample/Models/Customer.cs
+++ work/Training/WebApplicationExample/Models/Customer.cs
@@ -7,7 +7,7 @@
         public int Age { get; set; }
         public string Email { get; set; } = null!;
         public string PhoneNumber { get; set; }
-        public string Address { get; set; } = null!;
+        public List<Address> Addresses { get; set; } = new List<Address>();
         public List<Contact> contacts { get; set; } = new List<Contact>();
 
         public class Contact
@@ -16,5 +16,13 @@
             public string Type { get; set; }
             public string Detail { get; set; }
         }
+
+        public class Address
+        {
+            public int Id { get; set; }
+            public string Street { get; set; } = null!;
+            public string City { get; set; } = null!;
+        }
     }
 }
+
